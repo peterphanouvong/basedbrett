6f7cc05934706404f2d668c948b90f023ba000a3
Peter Phanouvong <peterphanouvong@gmail.com>
2024-08-09 11:42:45 +1000

    no href

--- src/main.jsx
+++ src/main.jsx
@@ -41,12 +41,12 @@ const handleRedirectCallback = (user, appState) => {
   console.log({ user, appState });
   if (appState?.redirectTo) {
     console.log("Redirecting to", appState.redirectTo);
-    // window.location.href = appState.redirectTo;
-    window.history.replaceState({}, document.title, appState.redirectTo);
+    window.location = appState.redirectTo;
+    // window.history.replaceState({}, document.title, appState.redirectTo);
   } else {
     console.log("Redirecting to /");
-    window.history.replaceState({}, document.title, window.location.pathname);
-    // window.location.href = "/";
+    // window.history.replaceState({}, document.title, window.location.pathname);
+    window.location = "/";
   }
 };
 
